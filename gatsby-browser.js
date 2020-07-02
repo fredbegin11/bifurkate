b310a4d0539d0c65e2a0e1f7b57cf446a0923431
Frédéric Bégin <fbegin@nexapp.ca>
2020-07-01 23:14:26 -0400

    Scrolling Menu

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './src/styles/index.scss';
+import 'simplebar/dist/simplebar.css';
 
 import { AthleteProvider } from './src/contexts/AthleteContext';
 import { MenuProvider } from './src/contexts/MenuContext';
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import classNames from 'classnames';
 import { FaCheck, FaTimes, FaPowerOff, FaBeer, FaEnvelope } from 'react-icons/fa';
 import MenuContext from '../../contexts/MenuContext';
+import SimpleBar from 'simplebar-react';
 
 const Menu = () => {
   const { isMenuOpen, setOption, options, setSeason } = useContext(MenuContext);
@@ -17,49 +18,51 @@ const Menu = () => {
   };
 
   return (
-    <div className={classNames('menu', isMenuOpen && '--open')}>
-      <div>
-        <div className="menu__block">
-          <span className="label__header --small-margin">Map Settings</span>
-          <button className="custom-button menu__item" onClick={() => setOption({ heatMapMode: !options.heatMapMode })}>
-            Heatmap Mode {options.heatMapMode ? <FaCheck className="menu__status --active" /> : <FaTimes className="menu__status --inactive" />}
-          </button>
-        </div>
-        <div className="menu__block">
-          <span className="label__header --small-margin">Activity Type</span>
-          <button className="custom-button menu__item" onClick={() => setOption({ showRide: !options.showRide })}>
-            Cycling Rides {options.showRide ? <FaCheck className="menu__status --active" /> : <FaTimes className="menu__status --inactive" />}
-          </button>
-          <button className="custom-button menu__item" onClick={() => setOption({ showRun: !options.showRun })}>
-            Runs {options.showRun ? <FaCheck className="menu__status --active" /> : <FaTimes className="menu__status --inactive" />}
-          </button>
-          <button className="custom-button menu__item" onClick={() => setOption({ showWalk: !options.showWalk })}>
-            Walks {options.showWalk ? <FaCheck className="menu__status --active" /> : <FaTimes className="menu__status --inactive" />}
-          </button>
-          <button className="custom-button menu__item" onClick={() => setOption({ showHike: !options.showHike })}>
-            Hikes {options.showHike ? <FaCheck className="menu__status --active" /> : <FaTimes className="menu__status --inactive" />}
-          </button>
-        </div>
-        <div className="menu__block">
-          <span className="label__header --small-margin">Seasons</span>
-          {Object.keys(options.seasons).map(key => (
-            <button key={key} className="custom-button menu__item" onClick={() => setSeason({ [key]: !options.seasons[key] })}>
-              {key} {options.seasons[key] ? <FaCheck className="menu__status --active" /> : <FaTimes className="menu__status --inactive" />}
+    <div className="menu__container">
+      <SimpleBar autoHide={false} className={classNames('menu', isMenuOpen && '--open')}>
+        <div className="menu__inner" style={{ maxHeight: 20 }}>
+          <div className="menu__block">
+            <span className="label__header --small-margin">Map Settings</span>
+            <button className="custom-button menu__item" onClick={() => setOption({ heatMapMode: !options.heatMapMode })}>
+              Heatmap Mode {options.heatMapMode ? <FaCheck className="menu__status --active" /> : <FaTimes className="menu__status --inactive" />}
             </button>
-          ))}
+          </div>
+          <div className="menu__block">
+            <span className="label__header --small-margin">Activity Type</span>
+            <button className="custom-button menu__item" onClick={() => setOption({ showRide: !options.showRide })}>
+              Cycling Rides {options.showRide ? <FaCheck className="menu__status --active" /> : <FaTimes className="menu__status --inactive" />}
+            </button>
+            <button className="custom-button menu__item" onClick={() => setOption({ showRun: !options.showRun })}>
+              Runs {options.showRun ? <FaCheck className="menu__status --active" /> : <FaTimes className="menu__status --inactive" />}
+            </button>
+            <button className="custom-button menu__item" onClick={() => setOption({ showWalk: !options.showWalk })}>
+              Walks {options.showWalk ? <FaCheck className="menu__status --active" /> : <FaTimes className="menu__status --inactive" />}
+            </button>
+            <button className="custom-button menu__item" onClick={() => setOption({ showHike: !options.showHike })}>
+              Hikes {options.showHike ? <FaCheck className="menu__status --active" /> : <FaTimes className="menu__status --inactive" />}
+            </button>
+          </div>
+          <div className="menu__block">
+            <span className="label__header --small-margin">Seasons</span>
+            {Object.keys(options.seasons).map(key => (
+              <button key={key} className="custom-button menu__item" onClick={() => setSeason({ [key]: !options.seasons[key] })}>
+                {key} {options.seasons[key] ? <FaCheck className="menu__status --active" /> : <FaTimes className="menu__status --inactive" />}
+              </button>
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="menu__footer">
-        <a href="mailto:frederic.begin.fb@gmail.com?subject=Bifurkate Feedback" target="_blank" rel="noopener noreferrer" className="custom-button menu__item">
-          Feedback / Suggestion <FaEnvelope className="menu__status" />
-        </a>
-        <a href="https://www.paypal.me/fredbegin11" target="_blank" rel="noopener noreferrer" className="custom-button menu__item">
-          Buy me a beer <FaBeer className="menu__status" />
-        </a>
-        <button className="custom-button menu__item" onClick={handleLogOffClick}>
-          Log off <FaPowerOff className="menu__status" />
-        </button>
-      </div>
+        <div className="menu__footer">
+          <a href="mailto:frederic.begin.fb@gmail.com?subject=Bifurkate Feedback" target="_blank" rel="noopener noreferrer" className="custom-button menu__item">
+            Feedback / Suggestion <FaEnvelope className="menu__status" />
+          </a>
+          <a href="https://www.paypal.me/fredbegin11" target="_blank" rel="noopener noreferrer" className="custom-button menu__item">
+            Buy me a beer <FaBeer className="menu__status" />
+          </a>
+          <button className="custom-button menu__item" onClick={handleLogOffClick}>
+            Log off <FaPowerOff className="menu__status" />
+          </button>
+        </div>
+      </SimpleBar>
     </div>
   );
 };
