1327baf38144fdc314ad9f823d75b2b0ce6fe88f
Frédéric Bégin <fbegin@nexapp.ca>
2020-07-06 07:54:39 -0400

    Added Line Weight

--- src/components/Menu/Blocks/MapOptions.jsx
+++ src/components/Menu/Blocks/MapOptions.jsx
@@ -3,6 +3,7 @@ import { FaCheck, FaTimes } from 'react-icons/fa';
 import { CirclePicker } from 'react-color';
 import { useState } from 'react';
 import Collapsable from '../Collapsable';
+import InputRange from 'react-input-range';
 
 const MapOptions = ({ mapConfig, setMapOption }) => {
   const [isColorPickerOpened, setIsColorPickerOpened] = useState(false);
@@ -10,7 +11,7 @@ const MapOptions = ({ mapConfig, setMapOption }) => {
   return (
     <Collapsable label="Map Options" isInitiallyOpen>
       <button className="custom-button menu__item" onClick={() => setMapOption({ heatMapMode: !mapConfig.heatMapMode })}>
-        Heatmap Mode {mapConfig.heatMapMode ? <FaCheck className="menu__status --active" /> : <FaTimes className="menu__status --inactive" />}
+        Heatmap {mapConfig.heatMapMode ? <FaCheck className="menu__status --active" /> : <FaTimes className="menu__status --inactive" />}
       </button>
       <div className="custom-button menu__item" onClick={() => setIsColorPickerOpened(!isColorPickerOpened)}>
         <span className="menu__color-picker">Line Color</span>
@@ -23,7 +24,20 @@ const MapOptions = ({ mapConfig, setMapOption }) => {
           onChangeComplete={({ hex }) => setMapOption({ polylineColor: hex })}
         />
       </div>
-      <button className="custom-button menu__item --small" onClick={() => setMapOption({ heatMapMode: false, polylineColor: 'red' })}>
+      <div className="custom-button menu__item" onClick={() => setIsColorPickerOpened(!isColorPickerOpened)}>
+        <span className="menu__weight-picker">Line Weight</span>
+        <div className="menu__weight-picker --picker">
+          <InputRange
+            maxValue={5}
+            minValue={1}
+            color={mapConfig.polylineColor}
+            value={mapConfig.polylineWeight}
+            onChange={polylineWeight => setMapOption({ polylineWeight })}
+            formatLabel={() => ''}
+          />
+        </div>
+      </div>
+      <button className="custom-button menu__item --small" onClick={() => setMapOption({ heatMapMode: false, polylineColor: 'red', polylineWeight: 2 })}>
         Reset to defaults
       </button>
     </Collapsable>
