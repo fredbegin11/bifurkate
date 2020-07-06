a6b1b9c10de65cfcdb13b9202fb021fc788e8817
Frédéric Bégin <fbegin@nexapp.ca>
2020-07-06 08:43:13 -0400

    react-device-detect in useEffect

--- src/components/Menu/MenuWrapper.jsx
+++ src/components/Menu/MenuWrapper.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { isMobile } from 'react-device-detect';
 import SimpleBar from 'simplebar-react';
 import classNames from 'classnames';
+import { useEffect } from 'react';
 
 const MenuWrapper = ({ children, isMenuOpen }) => {
+  const [showMobile, setShowMobile] = useState(false);
+
+  useEffect(() => {
+    setShowMobile(isMobile);
+  }, []);
+
+  if (showMobile) {
+    return <div className={classNames('menu', isMenuOpen && '--open')}>{children}</div>;
+  }
+
   return (
     <SimpleBar forceVisible={true} autoHide={false} className={classNames('menu', isMenuOpen && '--open')}>
       {children}
