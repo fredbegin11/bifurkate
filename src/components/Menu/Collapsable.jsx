ff92b78529342b6f0d4b9438d9c863445787b236
Frédéric Bégin <fbegin@nexapp.ca>
2020-07-06 07:40:53 -0400

    Removed Collapsable Transition

--- src/components/Menu/Collapsable.jsx
+++ src/components/Menu/Collapsable.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import { useState } from 'react';
-import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
-import { CSSTransition } from 'react-transition-group';
 
 const Collapsable = ({ label, isInitiallyOpen, children }) => {
-  const [isOpen, setIsOpen] = useState(isInitiallyOpen);
+  //  const [isOpen, setIsOpen] = useState(isInitiallyOpen);
+
+  // if (isMobile) {
+  //   return (
+  //     <div className="menu__block">
+  //       <button className="custom-button menu__item" onClick={() => setIsOpen(!isOpen)}>
+  //         <div className="label__header --no-margin">{label}</div>
+  //         <FaChevronUp className={classNames('menu__status', !isOpen && '--down')} />
+  //       </button>
+  //       <CSSTransition in={isOpen} timeout={200} classNames="collapsable" unmountOnExit mountOnEnter>
+  //         <div className="collapsable">{children}</div>
+  //       </CSSTransition>
+  //     </div>
+  //   );
+  // }
 
   return (
     <div className="menu__block">
-      <button className="custom-button menu__item" onClick={() => setIsOpen(!isOpen)}>
-        <div className="label__header --no-margin">{label}</div>
-        {isOpen ? <FaChevronUp className="menu__status" /> : <FaChevronDown className="menu__status" />}
-      </button>
-      <CSSTransition in={isOpen} timeout={200} classNames="collapsable" unmountOnExit mountOnEnter>
-        <div className="collapsable">{children}</div>
-      </CSSTransition>
+      <div className="label__header --small-margin">{label}</div>
+
+      <div className="collapsable">{children}</div>
     </div>
   );
 };
