da9fdf61c9bf3b418306d0f8dfbf3626218c782e
Frédéric Bégin <fbegin@nexapp.ca>
2020-07-09 00:03:38 -0400

    minimy css

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,13 +3,16 @@ import './src/styles/index.scss';
 import 'simplebar/dist/simplebar.css';
 import 'react-input-range/lib/css/index.css';
 import 'react-dates/initialize';
+import { minify } from 'aphrodite';
 
 import { AthleteProvider } from './src/contexts/AthleteContext';
 import { MenuProvider } from './src/contexts/MenuContext';
 import ThemedStyleSheet from 'react-with-styles/lib/ThemedStyleSheet';
 import aphroditeInterface from 'react-with-styles-interface-aphrodite';
-// import DefaultTheme from 'react-dates/lib/theme/DefaultTheme';
 import DatesTheme from './src/themes/DatesTheme';
+
+// Always keep the full style names
+minify(false);
 
 ThemedStyleSheet.registerInterface(aphroditeInterface);
 ThemedStyleSheet.registerTheme(DatesTheme);
