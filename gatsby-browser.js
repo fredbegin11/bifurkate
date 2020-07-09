3c4abbd7ab9d52ba69a3d8e8fe70bc26735b6f1c
Frédéric Bégin <fbegin@nexapp.ca>
2020-07-08 23:29:52 -0400

    WIP Dark Theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,10 +3,16 @@ import './src/styles/index.scss';
 import 'simplebar/dist/simplebar.css';
 import 'react-input-range/lib/css/index.css';
 import 'react-dates/initialize';
-import 'react-dates/lib/css/_datepicker.css';
 
 import { AthleteProvider } from './src/contexts/AthleteContext';
 import { MenuProvider } from './src/contexts/MenuContext';
+import ThemedStyleSheet from 'react-with-styles/lib/ThemedStyleSheet';
+import aphroditeInterface from 'react-with-styles-interface-aphrodite';
+// import DefaultTheme from 'react-dates/lib/theme/DefaultTheme';
+import DatesTheme from './src/themes/DatesTheme';
+
+ThemedStyleSheet.registerInterface(aphroditeInterface);
+ThemedStyleSheet.registerTheme(DatesTheme);
 
 export const wrapRootElement = ({ element }) => (
   <MenuProvider>
