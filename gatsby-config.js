a6443dd6559b11f5029a28055f678250e8376c2e
Frédéric Bégin <fbegin@nexapp.ca>
2020-07-12 13:54:29 -0400

    trailing slash

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
         trackingId: 'UA-171159310-1',
       },
     },
+    `gatsby-plugin-remove-trailing-slashes`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
